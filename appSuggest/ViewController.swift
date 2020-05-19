7862acf07ad9d534a0d36450400ab40e2da5712b
Elviseas <elviseas@gmail.com>
2020-05-18 21:55:13 -0300

    Initial Commit

---
+++ appSuggest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appSuggest
+//
+//  Created by Elvis Silveira on 18/05/20.
+//  Copyright © 2020 Elvis Silveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
